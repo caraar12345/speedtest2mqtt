9a9fca795724f7c986c9240b9d0c7f5a95e31170
Aaron Carson <aaron@aaroncarson.co.uk>
2022-01-02 00:09:41 +0000

    Really messed that up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONFIG_PATH="/data/options.json"
-CRON=${`jq --raw-output '.cron // empty'`:-0 * * * *}
-echo "speedtest2mqtt has been started "
+CRON=${jq --raw-output '.cron // empty' $CONFIG_PATH:-"0 * * * *"}
+echo "speedtest2mqtt has been started"
 
 declare | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /var/tmp/container.env
 sed -i "/schedule/c\    schedule: \"${CRON}\"" /home/foo/crontab.yml
